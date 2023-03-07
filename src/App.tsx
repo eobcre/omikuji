66904c854d1f250dad476f4eba49891590c824c5
eobcre <eobcre@gmail.com>
2023-03-07 10:33:58 -0500

    button function

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { useState } from 'react';
 function App() {
   const [omikuji, setOmikuji] = useState('Draw a fortune slip.');
 
+  const clickHandler = () => {};
+
   return <div></div>;
 }
 
