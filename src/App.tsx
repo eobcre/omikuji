97154c70a866729545dc92a1803d0e3cc1f60772
eobcre <eobcre@gmail.com>
2023-03-07 10:59:24 -0500

    main created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { useState } from 'react';
+import MainIcon from './assets/omikuji.png';
 
 function App() {
   const [omikuji, setOmikuji] = useState('Draw a fortune slip.');
 
   const clickHandler = () => {};
 
-  return <div></div>;
+  return (
+    <div className='main'>
+      <img src={MainIcon} alt='Icon' width='70' />
+      <h2>Omikuji</h2>
+      <button>Draw</button>
+    </div>
+  );
 }
 
 export default App;
