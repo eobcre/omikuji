1f08ae2d22839a604b24981903b2c153a0e0cad3
eobcre <eobcre@gmail.com>
2023-03-07 17:45:31 -0500

    file name changed

---
+++ src/utils/data.tsx
@@ -0,0 +1,23 @@
+// List
+export const data = [
+  {
+    name: '大吉',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi repudiandae rem quibusdam odio, aperiam, accusantium dolorem autem sed temporibus deserunt minima corporis tempore illum? Vel cupiditate voluptatem quam nemo veniam.',
+  },
+  {
+    name: '吉',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nihil quasi voluptatem saepe in rem quas nobis enim praesentium repellat inventore, odio dicta asperiores, veniam tempora quaerat esse numquam ducimus.',
+  },
+  {
+    name: '中吉',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nihil quasi voluptatem saepe in rem quas nobis enim praesentium repellat inventore, odio dicta asperiores, veniam tempora quaerat esse numquam ducimus.',
+  },
+  {
+    name: '小吉',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nihil quasi voluptatem saepe in rem quas nobis enim praesentium repellat inventore, odio dicta asperiores, veniam tempora quaerat esse numquam ducimus.',
+  },
+];
