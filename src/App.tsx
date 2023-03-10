5ed194f32c218dd5919a8cd59d6b0b11ea850d6d
eobcre <eobcre@gmail.com>
2023-03-10 14:54:01 -0500

    new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
-import { useState } from 'react';
-import { data } from './utils/data';
-// Icon
-import MainIcon from './assets/omikuji.png';
+import { Title } from './components/Title';
+import { Button } from './components/Button';
 
-function App() {
-  const [omikuji, setOmikuji] = useState({ name: '', description: '' });
-
-  // Click function
-  const clickHandler = () => {
-    const randomIndex = Math.floor(Math.random() * data.length);
-    setOmikuji(data[randomIndex]);
-  };
-
+const App = () => {
   return (
     // JSX
     <div className='main'>
-      <img src={MainIcon} alt='Icon' width='100' />
-      <h2>Omikuji</h2>
-      <button onClick={clickHandler}>Draw</button>
-      {omikuji && (
-        <div className='result'>
-          <h1>{omikuji.name}</h1>
-          <p>{omikuji.description}</p>
-        </div>
-      )}
+      <Title />
+      <Button />
     </div>
   );
-}
+};
 
 export default App;
