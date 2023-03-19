b8cbf40d43935b54ef001d7e841a69e06ee2c540
eobcre <eobcre@gmail.com>
2023-03-19 07:02:02 -0400

    button component props settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 // components
 import { Title } from './components/Title';
 import { Button } from './components/Button';
+import { Result } from './components/Result';
+// utils
+import { data } from './utils/data';
+import { Omikuji, ButtonProps } from './utils/type';
 
 const App = () => {
+  const [omikuji, setOmikuji] = useState<Omikuji>({
+    name: '',
+    description: '',
+  });
   return (
     // JSX
     <div className='main'>
       <Title title='Omikuji' />
-      <Button name='Draw' />
+      <Button name='Draw' setOmikuji={setOmikuji} />
     </div>
   );
 };
