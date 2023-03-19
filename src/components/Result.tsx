33eeec2a769b6d04ae368764d6a5b55ad6edc5e2
eobcre <eobcre@gmail.com>
2023-03-19 07:22:29 -0400

    moved JSX, props settings

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,3 +1,15 @@
-export const Result = () => {
-  return <div>Result</div>;
+// utils
+import { ResultProps } from '../utils/type';
+
+export const Result = (props: ResultProps) => {
+  return (
+    <div>
+      {props.omikuji && (
+        <div className='result'>
+          <h1>{props.omikuji.name}</h1>
+          <p>{props.omikuji.description}</p>
+        </div>
+      )}
+    </div>
+  );
 };
