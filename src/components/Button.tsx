e04aa6492ab2204a39c36ae5177b295b98455870
eobcre <eobcre@gmail.com>
2023-03-19 07:05:34 -0400

    moved useState to App, JSX to Result, clickHandler fixed

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,31 +1,18 @@
-import { useState } from 'react';
 // utils
 import { data } from '../utils/data';
 import { ButtonProps, Omikuji } from '../utils/type';
 
 export const Button = (props: ButtonProps) => {
-  const [omikuji, setOmikuji] = useState<Omikuji>({
-    name: '',
-    description: '',
-  });
-
   // click
   const clickHandler = () => {
     const randomIndex = Math.floor(Math.random() * data.length);
-    setOmikuji(data[randomIndex]);
+    const omikujiNum: Omikuji = data[randomIndex];
+    props.setOmikuji(omikujiNum);
   };
   return (
     // JSX
     <>
       <button onClick={clickHandler}>{props.name}</button>
-      <div>
-        {omikuji && (
-          <div className='result'>
-            <h1>{omikuji.name}</h1>
-            <p>{omikuji.description}</p>
-          </div>
-        )}
-      </div>
     </>
   );
 };
